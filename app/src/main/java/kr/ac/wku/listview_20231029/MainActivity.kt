17a9ae4d0df77e36149a5c0a7672498307f370cc
pky2119 <pky2119@naver.com>
2023-10-29 11:48:27 +0900

    어댑터 객체생성 리스트뷰 연결 화면에 기본 모양은 출력

--- app/src/main/java/kr/ac/wku/listview_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/listview_20231029/MainActivity.kt
@@ -2,14 +2,21 @@ package kr.ac.wku.listview_20231029
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.listview_20231029.adapters.StudentAdapters
+import kr.ac.wku.listview_20231029.databinding.ActivityMainBinding
 import kr.ac.wku.listview_20231029.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
 
     val mStudentList = ArrayList<StudentData>()
+
+    lateinit var mStdAdapter: StudentAdapters
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 //        화면이 켜질 때 학생 목록을 ArrayList에 수기로 추가 (임시 활용)
 
@@ -19,6 +26,12 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add( StudentData("최땡땡", 1994, "010-0123-4567") )
         mStudentList.add( StudentData("홍땡땡", 1995, "010-8901-2345") )
         mStudentList.add( StudentData("민땡땡", 1996, "010-6789-0123") )
+
+//        어댑터 변수도 객체 생성
+
+        mStdAdapter = StudentAdapters(this, R.layout.student_list_item, mStudentList)
+
+        binding.studentListView.adapter = mStdAdapter
 
     }
 }
